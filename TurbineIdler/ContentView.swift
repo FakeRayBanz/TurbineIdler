7219880de94aabb506c90da406b906d464e53c4d
Matthew Parker <matthewparker@hotmail.com.au>
2022-06-14 20:15:05 +1000

    Initial Commit

---
+++ TurbineIdler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TurbineIdler
+//
+//  Created by Matthew Parker on 14/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TurbineIdler/TurbineIdlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurbineIdlerApp.swift
+//  TurbineIdler
+//
+//  Created by Matthew Parker on 14/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct TurbineIdlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
